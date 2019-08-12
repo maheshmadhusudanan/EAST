30afcc8ace1a036ba3ccd90778c6c1ec3fc6865b
maheshmad <mahesh.madhusudanan@gmail.com>
2019-08-12 07:54:35 -0400

    updates

--- eval.py
+++ eval.py
@@ -5,21 +5,25 @@ import os
 import argparse
 import numpy as np
 import tensorflow as tf
+import sys
+from os import path
 from keras.models import load_model, model_from_json
-
 import locality_aware_nms as nms_locality
 import lanms
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--test_data_path', type=str, default='../data/ICDAR2015/test_data')
 parser.add_argument('--gpu_list', type=str, default='0')
-parser.add_argument('--model_path', type=str, default='')
+parser.add_argument('--model_path', type=str, default='/models/EAST_IC15+13_model.h5')
 parser.add_argument('--output_dir', type=str, default='tmp/eval/east_icdar2015_resnet_v1_50_rbox/')
 FLAGS = parser.parse_args()
 
 from model import *
 from losses import *
 from data_processor import restore_rectangle
+
+sys.path.insert(0,'../deep-text-recognition-benchmark')
+from text_reader import TextReader
 
 def get_images():
     '''
@@ -122,9 +126,42 @@ def sort_poly(p):
         return p[[0, 3, 2, 1]]
 
 
+def get_avg_font_size(boxes):
+    
+    w = []
+    h = []
+
+    for box in boxes:
+        
+        print("about to calculate mean for box")
+        # to avoid submitting errors
+        
+        box = sort_poly(box.astype(np.int32))
+        
+        y1 = box[0,1]
+        y2 = box[2,1]
+        x1 = box[0,0]
+        x2 = box[2,0]
+
+        w.append(x2 - x1)
+        h.append(y2 - y1)
+    
+    meanw = np.mean(w)
+    meanh = np.mean(h)
+
+    print("mean w = " + meanw)
+    print("mean h = " + meanh)
+
+    return meanw, meanh
+    
+
+
+
 def main(argv=None):
     import os
     os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu_list
+    path = os.path.abspath(__file__)
+    dir_path = os.path.dirname(path)
 
     try:
         os.makedirs(FLAGS.output_dir)
@@ -133,11 +170,12 @@ def main(argv=None):
             raise
 
     # load trained model
-    json_file = open(os.path.join('/'.join(FLAGS.model_path.split('/')[0:-1]), 'model.json'), 'r')
+    json_file = open(dir_path+'/models/model.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json, custom_objects={'tf': tf, 'RESIZE_FACTOR': RESIZE_FACTOR})
-    model.load_weights(FLAGS.model_path)
+    model.load_weights(dir_path+FLAGS.model_path)
+    print("**** loading "+dir_path+FLAGS.model_path+"......successful *******")
 
     img_list = get_images()
     for img_file in img_list:
@@ -148,7 +186,7 @@ def main(argv=None):
         img_resized = (img_resized / 127.5) - 1
 
         timer = {'net': 0, 'restore': 0, 'nms': 0}
-        start = time.time()
+        start = time.time() 
 
         # feed image into model
         score_map, geo_map = model.predict(img_resized[np.newaxis, :, :, :])
@@ -158,6 +196,9 @@ def main(argv=None):
         boxes, timer = detect(score_map=score_map, geo_map=geo_map, timer=timer)
         print('{} : net {:.0f}ms, restore {:.0f}ms, nms {:.0f}ms'.format(
             img_file, timer['net']*1000, timer['restore']*1000, timer['nms']*1000))
+
+        print("getting the mean!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        #meanw, meanh = get_avg_font_size(boxes)
 
         if boxes is not None:
             boxes = boxes[:, :8].reshape((-1, 4, 2))
@@ -174,17 +215,47 @@ def main(argv=None):
                 '{}.txt'.format(
                     os.path.basename(img_file).split('.')[0]))
 
+            #textReader = TextReader({})
+            phase2_img_path = os.path.join(FLAGS.output_dir ,"phase2")
+            i = 0
             with open(res_file, 'w') as f:
                 for box in boxes:
                     # to avoid submitting errors
                     box = sort_poly(box.astype(np.int32))
                     if np.linalg.norm(box[0] - box[1]) < 5 or np.linalg.norm(box[3]-box[0]) < 5:
                         continue
-                    f.write('{},{},{},{},{},{},{},{}\r\n'.format(
-                        box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1],
-                    ))
-                    cv2.polylines(img[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
 
+                    #
+                    # Detect the text inside 
+                    #
+                    # print ("******** for Box = "+ str(box[0, 0]) +","+str(box[0, 1])+" -- "+
+                    #                     str(box[1, 0])+","+str(box[1, 1])+" -- "+
+                    #                     str(box[2, 0])+","+str(box[2, 1])+" -- "+
+                    #                     str(box[3, 0])+","+str(box[3, 1])
+                    # )
+                    y1 = box[0,1]
+                    y2 = box[2,1]
+                    x1 = box[0,0]
+                    x2 = box[2,0]
+                    crop_img = img[y1:y2, x1:x2]                    
+                    if crop_img.size != 0:
+                       phase2_temp_img_path = phase2_img_path + "/" +str(i) + "_" + os.path.basename(img_file)
+                       cv2.imwrite(phase2_temp_img_path, crop_img)
+                       i += 1
+                       #print (txtReader.predict(crop_img))
+                       print("writing file ... "+phase2_temp_img_path)
+                    #
+                    # write output
+                    #
+                    #f.write('{},{},{},{},{},{},{},{}\r\n'.format(
+                    #    box[0, 0], box[0, 1], box[1, 0], box[1, 1], box[2, 0], box[2, 1], box[3, 0], box[3, 1],
+                    #))
+                    #cv2.polylines(img[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], True, color=(255, 255, 0), thickness=1)
+                    # cv2.fillPoly(img[:, :, ::-1], [box.astype(np.int32).reshape((-1, 1, 2))], color=(255, 255, 255))
+                    # cv2.rectangle(img, (x1,y1), (x2,y2), color=(0, 0, 255), cv2.FILLED)
+
+
+    
 
         img_path = os.path.join(FLAGS.output_dir, os.path.basename(img_file))
         cv2.imwrite(img_path, img[:, :, ::-1])
